8a3a11097f965c9ebe0d29d3e64dd32a51b3898b
양혜지 <haejidev@gmail.com>
2021-10-13 22:24:26 +0900

    Make BeerTable sizing to arr

---
+++ BeerGCD/BeerListCell.swift
@@ -0,0 +1,8 @@
+//
+//  BeerListCell.swift
+//  BeerGCD
+//
+//  Created by 양혜지 on 2021/10/13.
+//
+
+import Foundation
--- BeerGCD/BeerListViewController.swift
+++ BeerGCD/BeerListViewController.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 class BeerListViewController: UITableViewController {
+    var beerList = [Beer]()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UINavigationBar
+        
+        title = "패캠프루어리"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+
+// UITableView DataSource, Delegate
+extension BeerListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beerList.count
+    }
 }
